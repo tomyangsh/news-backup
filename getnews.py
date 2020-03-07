1e933068ab8dbc9298fe0933c0754289d2c37a3d
tomyang001 <wyh.aaron@gmail.com>
2020-03-07 14:15:16 -0500

    Add Matters

--- getnews.py
+++ getnews.py
@@ -2,17 +2,26 @@
 
 import feedparser
 import re
-#import html2text
 
+#China Digital Times
 a = feedparser.parse('https://chinadigitaltimes.net/chinese/feed/')
-#h = html2text.HTML2Text()
-#h.ignore_links = True
 
 for post in a.entries:
 	d = post.published_parsed
 	t = post.title+'.txt'
 	c = re.sub(r'\\n', '\n', str(post.content))
-#	cc = h.handle(c)
 	f = open(str(d[0])+'-'+str(d[1])+'/'+t, "w+")
 	f.write(re.sub(r"\[\{'type': 'text/html', 'language': None, 'base': 'https://chinadigitaltimes\.net/chinese/feed/', 'value': '|<.*?>|&nbsp;|&copy(.*\n)*|Feed.*", '', c))
 	f.close()
+
+#Matters
+a = feedparser.parse('http://155.138.236.181:1200/matters/latest')
+
+for post in a.entries:
+	d = post.published_parsed
+	t = post.title+'.txt'
+	c = re.sub(r'</p><p>', '\n', str(post.description))
+	f = open(str(d[0])+'-'+str(d[1])+'/'+t, "w+")
+	f.write(re.sub(r"<.*?>", '', c))
+	f.close()
+
