47eb3de07eff0ef693d5b53270dab5caaeab6a89
tomyang001 <wyh.aaron@gmail.com>
2020-10-03 16:45:26 -0400

    fix content error

--- getnews.py
+++ getnews.py
@@ -9,19 +9,8 @@ a = feedparser.parse('https://chinadigitaltimes.net/chinese/feed/')
 for post in a.entries:
 	d = post.published_parsed
 	t = post.title+'.txt'
-	c = re.sub(r'\\n', '\n', str(post.content))
+	c = re.sub(r'\\n', '\n', str(post.summary))
 	f = open(str(d[0])+'-'+str(d[1])+'/'+t, "w+")
 	f.write(re.sub(r"\[\{'type': 'text/html', 'language': None, 'base': 'https://chinadigitaltimes\.net/chinese/feed/', 'value': '|<.*?>|&nbsp;|&copy(.*\n)*|Feed.*", '', c))
-	f.close()
-
-#Matters
-a = feedparser.parse('http://155.138.236.181:1200/matters/latest')
-
-for post in a.entries:
-	d = post.published_parsed
-	t = post.title+'.txt'
-	c = re.sub(r'</p><p>', '\n', str(post.description))
-	f = open(str(d[0])+'-'+str(d[1])+'/'+t, "w+")
-	f.write(re.sub(r"<.*?>", '', c))
 	f.close()
 
