60584cfbcdaad20dd3f19630f35a0c3f750b8786
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-08-14 23:57:19 -0400

    bypass content parse error

--- getnews.py
+++ getnews.py
@@ -15,14 +15,16 @@ for post in a.entries:
     time_published = post.published_parsed
     title = post.title + '.md'
     content = post.content[0].value
-    soup = BeautifulSoup(content, 'html.parser')
-    soup.a.decompose()
-    if soup.find_all('div', {'class': 'su-spoiler-title'}):
-        soup.find_all('div', {'class': 'su-spoiler-title'})[0].decompose()
-    md = re.sub(r'(\*\*.+\*\*)', r'\1 ', markdownify.markdownify(str(soup)))
-    directory = str(time.strftime("%Y-%m", time_published))
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    with open(directory + '/' + title, "w") as file:
-        file.write(md)
-
+    try:
+        soup = BeautifulSoup(content, 'html.parser')
+        soup.a.decompose()
+        if soup.find_all('div', {'class': 'su-spoiler-title'}):
+            soup.find_all('div', {'class': 'su-spoiler-title'})[0].decompose()
+        md = re.sub(r'(\*\*.+\*\*)', r'\1 ', markdownify.markdownify(str(soup)))
+        directory = str(time.strftime("%Y-%m", time_published))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(directory + '/' + title, "w") as file:
+            file.write(md)
+    except:
+        continue
